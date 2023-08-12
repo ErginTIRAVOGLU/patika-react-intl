5388a74ad0a756230d9c41f3284160dbdf2d662c
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-08-12 21:05:23 +0300

    intl Tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { IntlProvider, FormattedMessage, FormattedNumber } from "react-intl";
+
+const messages = {
+  "tr-TR": {
+    title: "Merhaba Dünya",
+    description: "{count} Yeni Mesajınız var",
+  },
+  "en-US": { 
+    title: "Hello World",
+    description: "You have {count} new message",
+  },
+};
 
 function App() {
+
+  const defaultLocale = localStorage.getItem("lang") ? localStorage.getItem("lang") : navigator.language;
+  const [lang, setLang] = useState(defaultLocale);
+
+  useEffect(()=> {
+    localStorage.setItem("lang",lang);
+  },[lang]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider messages={messages[lang]} locale={lang}>
+      <FormattedMessage id="title" />
+      <br />
+      <p>
+        <FormattedMessage id="description" values={{count:4}} />
+      </p>
+      <br />
+      <button onClick={() => setLang("tr-TR")}>TR</button>
+      <button onClick={() => setLang("en-US")}>EN</button>
+    </IntlProvider>
   );
 }
 
